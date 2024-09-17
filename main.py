e655a80a9fe2e3834942b247f8792c3aeb27498a
vanyaxromow <vanya.xromow@yandex.ru>
2024-09-17 23:28:59 +0300

    17.09

---
+++ main.py
@@ -0,0 +1,37 @@
+# Пункты задачи:
+# Создайте класс Knight с соответствующими описанию свойствами.
+# Создайте и запустите 2 потока на основе класса Knight.
+# Выведите на экран строку об окончании битв.
+
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        suppostat = 100
+        fl = 0
+        while suppostat != 0:
+            suppostat -= self.power
+            fl += 1
+            sleep(1)
+            print(f"{self.name} сражается {fl} день(дня)...., осталось {suppostat} воинов.")
+        print(f"{self.name} одержал победу спустя {fl} дней(дня)!")
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+print(f'Все битвы закончились!')
